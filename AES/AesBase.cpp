52fb95fb764b3fc38093cc86e7bdde9778b829bc
Dawid Janik <jawiddanik94@gmail.com>
2018-12-30 22:07:09 +0100

    AesBase added, some decryption methods implemented

---
+++ AES/AesBase.cpp
@@ -0,0 +1,98 @@
+#include "AesBase.h"
+
+
+
+int AesBase::getSBoxValue(int n)
+{
+	return sbox[n];
+}
+
+void AesBase::keyExpansion()
+{
+	int i, j;
+	byte temp[4], k;
+
+	// The first round key is the key itself.
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			round_key[i][j] = key[i][j];
+		}
+	}
+
+	while (j < (Nr * 4)) {
+
+		// Copying the previous column
+		for (i = 0; i < 4; i++) {
+			temp[i] = round_key[i][j - 1];
+		}
+
+		if (i % Nk == 0) {
+			// This function rotates the 4 bytes in a word to the left once.
+			// [a0,a1,a2,a3] becomes [a1,a2,a3,a0]
+
+			// Function RotWord()
+			k = temp[0];
+			temp[0] = temp[1];
+			temp[1] = temp[2];
+			temp[2] = temp[3];
+			temp[3] = k;
+
+			// SubWord() is a function that takes a four-byte input word and 
+			// applies the S-box to each of the four bytes to produce an output word.
+
+			// Function Subword()
+			temp[0] = getSBoxValue(temp[0]);
+			temp[1] = getSBoxValue(temp[1]);
+			temp[2] = getSBoxValue(temp[2]);
+			temp[3] = getSBoxValue(temp[3]);
+
+			// Column XOR round constant
+			temp[0] = temp[0] ^ rcon[i / Nk];
+		}
+
+		else if (Nk > 6 && i % Nk == 4) {
+			// Function Subword()
+			temp[0] = getSBoxValue(temp[0]);
+			temp[1] = getSBoxValue(temp[1]);
+			temp[2] = getSBoxValue(temp[2]);
+			temp[3] = getSBoxValue(temp[3]);
+		}
+		round_key[0][j] = round_key[0][j - Nk] ^ temp[0];
+		round_key[1][j] = round_key[1][j - Nk] ^ temp[1];
+		round_key[2][j] = round_key[2][j - Nk] ^ temp[2];
+		round_key[3][j] = round_key[3][j - Nk] ^ temp[3];
+		j++;
+	}
+}
+
+void AesBase::addRoundKey(int round, byte block[4][4])
+{
+	int i, j;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			block[i][j] ^= round_key[i][round * 4 + j];
+		}
+	}
+}
+
+bool AesBase::loadBlock(Text& input, byte block[4][4], int block_num)
+{
+
+}
+
+void AesBase::saveBlock(byte * dest, byte block[4][4], int block_num)
+{
+}
+
+void AesBase::loadKeyToMatrix(Text & key_file)
+{
+}
+
+AesBase::AesBase()
+{
+}
+
+
+AesBase::~AesBase()
+{
+}
--- AES/DecryptionService.cpp
+++ AES/DecryptionService.cpp
@@ -1,6 +1,80 @@
 ﻿#include "DecryptionService.h"
 #include "Key.h"
 
+int DecryptionService::getSBoxDecrypt(int n)
+{
+	return rsbox_decrypt[n];
+}
+
+void DecryptionService::mixColumns(byte block[4][4])
+{
+}
+
+void DecryptionService::subBytes(byte block[4][4])
+{
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			block[i][j] = getSBoxDecrypt(block[i][j]);
+		}
+	}
+}
+
+void DecryptionService::shiftRows(byte block[4][4])
+{
+	byte temp;
+
+	// Rotate first row 1 columns to right
+	temp = block[1][3];
+	block[1][3] = block[1][2];
+	block[1][2] = block[1][1];
+	block[1][1] = block[1][0];
+	block[1][0] = temp;
+
+	// Rotate second row 2 columns to right
+	temp = block[2][0];
+	block[2][0] = block[2][2];
+	block[2][2] = temp;
+
+	temp = block[2][1];
+	block[2][1] = block[2][3];
+	block[2][3] = temp;
+
+	// Rotate third row 3 columns to right
+	temp = block[3][0];
+	block[3][0] = block[3][1];
+	block[3][1] = block[3][2];
+	block[3][2] = block[3][3];
+	block[3][3] = temp;
+}
+
+void DecryptionService::decipher(byte block[4][4])
+{
+	int round;
+
+	addRoundKey(Nr, block);
+
+	// There will be Nr rounds.
+	// The first Nr-1 rounds are identical.
+	// These Nr-1 rounds are executed in the loop below.
+	for (round = Nr - 1; round > 0; round--)
+	{
+		shiftRows(block);
+		subBytes(block);
+		addRoundKey(round, block);
+		mixColumns(block);
+	}
+
+	// The last round is given below.
+	// The MixColumns function is not here in the last round.
+	shiftRows(block);
+	subBytes(block);
+	addRoundKey(0, block);
+
+}
+
 DecryptionService& DecryptionService::getInstance()
 {
 	static DecryptionService decryptionService;
@@ -11,3 +85,4 @@ void DecryptionService::decrypt(Key key, Text& text, int numberOfThreads)
 {
 
 }
+
